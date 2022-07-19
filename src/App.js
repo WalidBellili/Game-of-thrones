21747f6e59a4a8b8a11aa158432fcdcc1c81d042
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-19 17:28:41 +0200

    until exo 5 done

--- src/App.js
+++ src/App.js
@@ -13,13 +13,25 @@ class App extends React.Component {
     const request = await fetch("https://thronesapi.com/api/v2/Characters");
     // console.log(request);
     const response = await request.json();
-    // console.log(response);
+    console.log(response);
+    this.setState({
+      characters: response,
+    });
   }
   render() {
+    console.log(this.state);
     return (
-      <div>
+      <div className="d-flex flex-wrap align-items-center justify-content-center mb-3 text-center">
         <h1>Game of thrones</h1>;
-        <Character />
+        {this.state.characters.map((character) => {
+          return (
+            <Character
+              name={character.fullName}
+              title={character.title}
+              image={character.imageUrl}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -2,7 +2,22 @@ import React from "react";
 
 class Character extends React.Component {
   render() {
-    return <p>Character</p>;
+    return (
+      <div>
+        {/* <p>{this.props.name}</p>
+        <p>{this.props.title}</p>
+
+        <img src={this.props.image} alt="" width={330} height={190} /> */}
+
+        <div className="card">
+          <img src={this.props.image} alt="" width={280} height={190} />
+
+          <p>{this.props.name}</p>
+
+          <p>{this.props.title}</p>
+        </div>
+      </div>
+    );
   }
 }
 
