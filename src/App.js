a66a121926329333fa53537206630bf5063e216a
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-11 18:02:17 +0200

    convert the react class in react functions, first fetch for display them all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Characters from "./pages/Characters";
-import Favorites from "./pages/Favorites";
-import "./styles/layouts/index.css";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Characters />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,18 @@
 import { useEffect, useState } from "react";
-import Card from "../components/Card";
 
-const Characters = () => {
+const Home = () => {
   const [characters, setCharacters] = useState([]);
   useEffect(() => {
     fetchCharacters();
   }, []);
+
   const fetchCharacters = async () => {
     const request = await fetch(`https://thronesapi.com/api/v2/Characters`);
     const response = await request.json();
     setCharacters(response);
   };
-  if (!characters) {
-    <p>Loading...</p>;
-  }
-  return (
-    <main>
-      {characters.map((character) => {
-        return <Card character={character} />;
-      })}
-    </main>
-  );
+  console.log(characters);
+  return <div></div>;
 };
 
-export default Characters;
+export default Home;
